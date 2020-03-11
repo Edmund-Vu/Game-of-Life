598439b4e1c7f286589156ab69513fa481b7d0e7
Edmund <edmundvu816@gmail.com>
2020-03-11 19:18:35 +0000

    Wrote up the methods for classic mode

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -6,37 +6,19 @@
 
 using namespace std;
 
-settings::settings(){
+//constructors and destructors
+settings::settings(){}
+settings::~settings(){}
 
-}
+classicMode::classicMode(){}
+classicMode::~classicMode(){}
 
-settings::~settings(){
+mirrorMode::mirrorMode(){}
+mirrorMode::~mirrorMode(){}
 
-}
+donutMode::donutMode(){}
+donutMode::~donutMode(){}
 
-classicMode::classicMode(){
-
-}
-
-classicMode::~classicMode(){
-
-}
-
-mirrorMode::mirrorMode(){
-
-}
-
-mirrorMode::~mirrorMode(){
-
-}
-
-donutMode::donutMode(){
-
-}
-
-donutMode::~donutMode(){
-
-}
 
 void settings::prompt(){
   //strings to hold the string value of the row/column counts from the file
@@ -84,18 +66,20 @@ void settings::prompt(){
         }
       }
       //convert the strings read from the file into ints
-      rows = stoi(fileRows);
-      columns = stoi(fileColumns);
+      rowTotal = stoi(fileRows);
+      columnTotal = stoi(fileColumns);
       iStream.close();
+      createBoard();
       exit = false;
     }
     else if(boardType == "random"){
       cout << "Rows: ";
-      cin >> rows;
+      cin >> rowTotal;
       cout << "Columns: ";
-      cin >> columns;
+      cin >> columnTotal;
       cout << "Density: ";
       cin >> density;
+      createBoard();
       exit = false;
     }
     else{
@@ -105,32 +89,32 @@ void settings::prompt(){
 }
 
 
-void classicMode::createBoard(){
-  currBoard = new char*[rows];
-  prevBoard = new char*[rows];
+void settings::createBoard(){
+  currBoard = new char*[rowTotal];
+  prevBoard = new char*[rowTotal];
 
-  for(int i = 0; i < rows; ++i){
-    currBoard[i] = new char[columns];
-    for(int j = 0; j < columns; ++j){
+  for(int i = 0; i < rowTotal; ++i){
+    currBoard[i] = new char[columnTotal];
+    for(int j = 0; j < columnTotal; ++j){
       currBoard[i][j] = '-';
     }
   }
 
-  for(int i = 0; i < rows; ++i){
-    prevBoard[i] = new char[columns];
-    for(int j = 0; j < rows; ++j){
+  for(int i = 0; i < rowTotal; ++i){
+    prevBoard[i] = new char[columnTotal];
+    for(int j = 0; j < rowTotal; ++j){
       prevBoard[i][j] = '-';
     }
   }
 
   if(boardType == "random"){
-    int tileTotal = rows * columns;
+    int tileTotal = rowTotal * columnTotal;
     //cast to an int to get a whole number of live cells
     int xTotal = int(tileTotal * density);
 
     for(int i = 0; i < xTotal; ++i){
-      int randRow = rand() % rows;
-      int randColumn = rand() % columns;
+      int randRow = rand() % rowTotal;
+      int randColumn = rand() % columnTotal;
       if(currBoard[randRow][randColumn] == '-'){
         currBoard[randRow][randColumn] = 'X';
       }
@@ -153,11 +137,319 @@ void classicMode::createBoard(){
         currBoard[rowCount][columnCount] = c;
         ++columnCount;
       }
-      else if(c == 'n'){
+      else if(c == '\n'){
         //reset at the end of each line to get correct indexes in future rows
         columnCount = 0;
         ++rowCount;
       }
     }
   }
+}
+
+
+int classicMode::checkTopLeft(int rows, int columns){
+  int neighborCount = 0;
+  if(prevBoard[rows][columns + 1] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows + 1][columns] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows + 1][columns + 1] == 'X'){
+    ++neighborCount;
+  }
+
+  return neighborCount;
+}
+
+
+int classicMode::checkBottomLeft(int rows, int columns){
+  int neighborCount = 0;
+  if(prevBoard[rows][columns + 1] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows - 1][columns + 1] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows - 1][columns] == 'X'){
+    ++neighborCount;
+  }
+
+  return neighborCount;
+}
+
+
+int classicMode::checkTopRight(int rows, int columns){
+  int neighborCount = 0;
+  if(prevBoard[rows + 1][columns] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows + 1][columns -1] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows][columns - 1] == 'X'){
+    ++neighborCount;
+  }
+
+  return neighborCount;
+}
+
+
+int classicMode::checkBottomRight(int rows, int columns){
+  int neighborCount = 0;
+  if(prevBoard[rows - 1][columns] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows - 1][columns - 1] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows][columns - 1] == 'X'){
+    ++neighborCount;
+  }
+
+  return neighborCount;
+}
+
+int classicMode::checkTop(int rows, int columns){
+  int neighborCount = 0;
+  if(prevBoard[rows][columns - 1] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows + 1][columns - 1] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows + 1][columns] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows + 1][columns + 1] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows][columns + 1] == 'X'){
+    ++neighborCount;
+  }
+
+  return neighborCount;
+}
+
+
+int classicMode::checkRight(int rows, int columns){
+  int neighborCount = 0;
+  if(prevBoard[rows - 1][columns] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows - 1][columns-1] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows][columns - 1] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows + 1][columns - 1] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows + 1][columns] == 'X'){
+    ++neighborCount;
+  }
+
+  return neighborCount;
+}
+
+
+int classicMode::checkBottom(int rows, int columns){
+  int neighborCount = 0;
+  if(prevBoard[rows][columns - 1] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows - 1][columns - 1] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows - 1][columns] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows - 1][columns + 1] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows][columns + 1] == 'X'){
+    ++neighborCount;
+  }
+
+  return neighborCount;
+}
+
+
+int classicMode::checkLeft(int rows, int columns){
+  int neighborCount = 0;
+  if(prevBoard[rows - 1][columns] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows - 1][columns + 1] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows][columns + 1] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows + 1][columns + 1] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows + 1][columns] == 'X'){
+    ++neighborCount;
+  }
+
+  return neighborCount;
+}
+
+
+int classicMode::checkMiddle(int rows, int columns){
+  int neighborCount = 0;
+  if(prevBoard[rows - 1][columns - 1] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows - 1][columns] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows - 1][columns + 1] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows][columns + 1] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows + 1][columns + 1] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows + 1][columns] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows + 1][columns - 1] == 'X'){
+    ++neighborCount;
+  }
+
+  if(prevBoard[rows][columns - 1] == 'X'){
+    ++neighborCount;
+  }
+
+  return neighborCount;
+}
+
+
+void classicMode::nextGen(){
+  int neighborCount;
+  for(int i = 0; i < rowTotal; ++i){
+    for(int j = 0; j < columnTotal; ++j){
+      prevBoard[i][j] = currBoard[i][j];
+    }
+  }
+
+  for(int k = 0; k < rowTotal; ++k){
+    for(int l = 0; l < columnTotal; ++l){
+      //reset the neighborCount with each iteration
+      neighborCount = 0;
+      if(prevBoard[k][l] == 'X'){
+        //check for top left or top right corners, else check top
+        if(k == 0){
+          if(l == 0){
+            neighborCount = checkTopLeft(k, l);
+          }
+          else if(l == (columnTotal - 1)){
+            neighborCount = checkTopRight(k, l);
+          }
+          else{
+            neighborCount = checkTop(k, l);
+          }
+        }
+      }
+      //check for bottom left or bottom right corners, else check bottom
+      else if(k == (rowTotal - 1)){
+        if(l == 0){
+          neighborCount = checkBottomLeft(k, l);
+        }
+        else if(l == (columnTotal - 1)){
+          neighborCount = checkBottomRight(k, l);
+        }
+        else{
+          neighborCount = checkBottom(k, l);
+        }
+      }
+      else if(l == 0){
+        //check whether it isn't a corner
+        if(k != 0 && k != (rowTotal - 1)){
+          neighborCount = checkLeft(k, l);
+        }
+      }
+      else if(l == (columnTotal - 1)){
+        if(k != 0 && k != (rowTotal - 1)){
+          neighborCount = checkRight(k, l);
+        }
+      }
+      else{
+        neighborCount = checkMiddle(k, l);
+      }
+      if(neighborCount <= 1){
+        currBoard[k][l] = '-';
+      }
+      else if(neighborCount == 3){
+        currBoard[k][l] = 'X';
+      }
+      else{
+        currBoard[k][l] = '-';
+      }
+    }
+  }
+}
+
+
+void classicMode::printGen(){
+  cout << "Generation " << genCount << endl;
+  for(int i = 0; i < rowTotal; ++i){
+    for(int j = 0; j < columnTotal; ++j){
+      cout << currBoard[i][j];
+    }
+  }
+  ++genCount;
+}
+
+
+bool classicMode::checkEmpty(){
+  bool isEmpty = false;
+  if(currBoard[0][0] == '-'){
+    for(int i = 0; i < rowTotal; ++i){
+      for(int j = 0; j < columnTotal; ++j){
+        if(currBoard[i][j] == '-'){
+          isEmpty = true;
+        }
+        else{
+          isEmpty = false;
+        }
+      }
+    }
+  }
+  return isEmpty;
 }
