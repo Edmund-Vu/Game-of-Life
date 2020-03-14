076f55c3a5db0a3a891d526f37bb91b2ddadf629
Edmund <edmundvu816@gmail.com>
2020-03-14 08:35:40 +0000

    added functions for each mode type, as well as variables for outputting

--- Assignment2.h
+++ Assignment2.h
@@ -5,52 +5,61 @@
 
 using namespace std;
 
-class settings{
+class gameOfLife{
   public:
-    settings();
-    ~settings();
-    int rows;
-    int columns;
+    gameOfLife();
+    ~gameOfLife();
+    int rowTotal;
+    int columnTotal;
+    int genCount = 0;
+    int oscillationCount = 0;
+    int patternLoopingCount = 0;
     float density;
     string boardType;
     string outputType;
     string fileName;
+    string gameMode;
+    string outputContent;
+    string outputFileName;
+
+    char** currBoard;
+    char** prevBoard;
+    char** secondPrevBoard;
 
     void prompt();
-};
-
-class classicMode : public settings{
-  public:
-    classicMode();
-    ~classicMode();
-
-    int genCount = 0;
-    char** prevBoard;
-    char** currBoard;
-
     void createBoard();
-    int checkCornerNW();
-    int checkCornerNE();
-    int checkCornerSW();
-    int checkCornerSE();
-    int checkTop();
-    int checkRight();
-    int checkBottom();
-    int checkLeft();
-
-    void nextGen();
+    //classic mode functions
+    int classicTopLeft(int row, int column);
+    int classicBottomLeft(int row, int column);
+    int classicTopRight(int row, int column);
+    int classicBottomRight(int row, int column);
+    int classicTop(int row, int column);
+    int classicRight(int row, int column);
+    int classicBottom(int row, int column);
+    int classicLeft(int row, int column);
+    int checkMiddle(int row, int column);
+    //mirror mode functions
+    int mirrorTopLeft(int row, int column);
+    int mirrorBottomLeft(int row, int column);
+    int mirrorTopRight(int row, int column);
+    int mirrorBottomRight(int row, int column);
+    int mirrorTop(int row, int column);
+    int mirrorRight(int row, int column);
+    int mirrorBottom(int row, int column);
+    int mirrorLeft(int row, int column);
+    //doughnut mode functions
+    int doughnutTopLeft(int row, int column);
+    int doughnutBottomLeft(int row, int column);
+    int doughnutTopRight(int row, int column);
+    int doughnutBottomRight(int row, int column);
+    int doughnutTop(int row, int column);
+    int doughnutRight(int row, int column);
+    int doughnutBottom(int row, int column);
+    int doughnutLeft(int row, int column);
+    //functions shared by all 3 game modes
     void printGen();
-    bool checkContinue();
-};
-
-class mirrorMode : public classicMode{
-  public:
-    mirrorMode();
-    ~mirrorMode();
-};
-
-class donutMode : public classicMode{
-  public:
-    donutMode();
-    ~donutMode();
+    void nextGen();
+    bool checkEmpty();
+    bool checkOscillation();
+    bool checkPatternLooping();
 };
